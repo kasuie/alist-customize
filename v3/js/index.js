b2fcc42577ab5327f162ef61af1f419aeb121539
kasuie <kezhou.chen@biz-united.com.cn>
2023-04-02 21:13:49 +0800

    fix:添加social dom

--- v3/js/index.js
+++ v3/js/index.js
@@ -2,10 +2,10 @@
  * @Author: kasuie
  * @Date: 2023-03-31 17:00:13
  * @LastEditors: kasuie
- * @LastEditTime: 2023-04-02 00:52:53
+ * @LastEditTime: 2023-04-02 20:15:39
  * @Description: 
  */
-let footer = false, desc = false;
+let footer = false, desc = false, homeIcon = false;
 
 const setFooter = () => {
     const target = document.querySelector(".footer > div");
@@ -23,10 +23,23 @@ const setDesc = () => {
         desc = true;
     }
 }
+// const setHomeIcon = () => {
+//     const homeIcon = document.querySelector("nav.nav > .hope-c-cPYwgm > li.hope-c-khZXrc");
+//     if (homeIcon) {
+//         const homeIconA = homeIcon.getElementsByClassName("nav-link")[0];
+//         homeIconA.innerHTML = "💙";
+//         homeIcon.addEventListener("click", (e) => {
+//             const _homeIconA = document.querySelector("nav.nav > .hope-c-cPYwgm > li.hope-c-khZXrc > a.nav-link");
+//             _homeIconA.innerHTML = "💙";
+//         })
+//         homeIcon = true;
+//     }
+// }
 
 let interval = setInterval(() => {
     setFooter();
     setDesc();
+    // setHomeIcon();
     if(footer && desc) {
         clearInterval(interval);
     }
